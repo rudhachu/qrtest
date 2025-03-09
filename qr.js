021f3ee9ac38af8f4f1fb027b42defac67997947
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-03-09 23:40:39 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,138 +1,131 @@
-const { PasteClient, Publicity, ExpireDate } = require("pastebin-api");
-pastebin = new PasteClient('Q80IAWeVRBgHkz5GVKCnwZmc0iudKVgk')
-const {makeid} = require('./id');
-const QRCode = require('qrcode');
+const { exec } = require("child_process");
+const uploadToPastebin = require('./pastebin');
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-let router = express.Router()
+let router = express.Router();
 const pino = require("pino");
-const {
-	default: makeWASocket,
-	useMultiFileAuthState,
-	jidNormalizedUser,
-	Browsers,
-	delay,
-	fetchLatestBaileysVersion,
-	makeInMemoryStore,
-} = require("@whiskeysockets/baileys");
 
-// List of available browser configurations
-const browserOptions = [
-		Browsers.macOS("Safari"),
-		Browsers.macOS("Desktop"),
-		Browsers.macOS("Firefox"),
-		Browsers.macOS("Opera"),
-];
+let { toBuffer } = require("qrcode");
+const path = require('path');
+const fs = require("fs-extra");
+const { Boom } = require("@hapi/boom");
 
-// Function to pick a random browser
-function getRandomBrowser() {
-		return browserOptions[Math.floor(Math.random() * browserOptions.length)];
+
+if (fs.existsSync("./temp")) {
+  fs.emptyDirSync(__dirname, "/temp");
 }
 
-function removeFile(FilePath) {
-	if (!fs.existsSync(FilePath)) return false;
-	fs.rmSync(FilePath, {
-		recursive: true,
-		force: true
-	})
-};
-
-const specificFiles = [
-		'creds.json',
-		'app-state-sync-key-AAAAAED1.json',
-		'pre-key-1.json',
-		'pre-key-2.json',
-		'pre-key-3.json',
-		'pre-key-5.json',
-		'pre-key-6.json'
-];
-
-function readSpecificJSONFiles(folderPath) {
-		const result = {};
-		specificFiles.forEach(file => {
-				const filePath = path.join(folderPath, file);
-				if (fs.existsSync(filePath)) {
-						const fileContent = fs.readFileSync(filePath, 'utf-8');
-						result[file] = JSON.parse(fileContent);
-				} else {
-						console.warn(`File not found: ${filePath}`);
-				}
-		});
-		return result;
-}
-
-const {
-	readFile
-} = require("node:fs/promises")
 router.get('/', async (req, res) => {
-	const id = makeid();
-	async function Getqr() {
-		const {
-			state,
-			saveCreds
-		} = await useMultiFileAuthState('./temp/' + id)
-		try {
-			let session = makeWASocket({
-				auth: state,
-				printQRInTerminal: false,
-				logger: pino({
-					level: "silent"
-				}),
-				browser: getRandomBrowser(), // Assign a random browser
-});
+  const {
+    default: makeWASocket,
+    useMultiFileAuthState,
+    Browsers,
+    delay,
+    DisconnectReason,
+    makeInMemoryStore,
+  } = require("@whiskeysockets/baileys");
+  const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) });
 
-			session.ev.on('creds.update', saveCreds)
-			session.ev.on("connection.update", async (s) => {
-				const {
-					connection,
-					lastDisconnect,
-					qr
-				} = s;
-const colors = ['#FFFFFF', '#FFFF00', '#00FF00', '#FF0000', '#0000FF', '#800080'];  // Array of colors
-const randomColor = colors[Math.floor(Math.random() * colors.length)];  // Pick a random color
+  async function Getqr() {
+    const { state, saveCreds } = await useMultiFileAuthState(__dirname, "/temp");
 
-if (qr) {
-  const buffer = await QRCode.toBuffer(qr, {
-    type: 'png',              // Output type (PNG)
-    color: {
-      dark: randomColor,      // Random dark color
-      light: '#00000000'      // Transparent background
-    },
-    width: 300,               // Adjust the size if needed
+    try {
+      let session = makeWASocket({
+        printQRInTerminal: false,
+        logger: pino({ level: "silent" }),
+        browser: Browsers.macOS("Desktop"),
+        auth: state
+      });
+
+      session.ev.on("connection.update", async (s) => {
+        const { connection, lastDisconnect, qr } = s;
+
+        if (qr) {
+          // Ensure the response is only sent once
+          if (!res.headersSent) {
+            res.setHeader('Content-Type', 'image/png');
+            try {
+              const qrBuffer = (await toBuffer(qr));  // Convert QR to buffer
+              res.end(qrBuffer);  // Send the buffer as the response
+              return; // Exit the function to avoid sending further responses
+            } catch (error) {
+              console.error("Error generating QR Code buffer:", error);
+              return; // Exit after sending the error response
+            }
+          }
+        }
+
+        if (connection === "open") {
+          await delay(3000);
+          let user = session.user.id;
+
+          //===========================================================================================
+          //===============================  SESSION ID    ===========================================
+          //===========================================================================================
+
+          const auth_path = "./temp/";
+          const credsFilePath = auth_path + "creds.json";
+
+          // Upload the creds.json file to Pastebin directly
+          const pastebinUrl = await uploadToPastebin(credsFilePath, 'creds.json', 'json', '1');
+          
+          const Scan_Id = pastebinUrl;  // Use the returned Pastebin URL directly
+
+          console.log(`
+====================  SESSION ID  ==========================
+SESSION-ID ==> ${Scan_Id}
+-------------------   SESSION CLOSED   -----------------------
+`);
+
+          let sessionMsg = await session.sendMessage(user, { text: Scan_Id });
+          const qrMsg = `\n*ᴅᴇᴀʀ ᴜsᴇʀ ᴛʜɪs ɪs ʏᴏᴜʀ sᴇssɪᴏɴ ɪᴅ*\n\n◕ ⚠️ *ᴘʟᴇᴀsᴇ ᴅᴏ ɴᴏᴛ sʜᴀʀᴇ ᴛʜɪs ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ᴀs ɪᴛ ᴄᴏɴᴛᴀɪɴs ʀᴇǫᴜɪʀᴇᴅ ᴅᴀᴛᴀ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ᴄᴏɴᴛᴀᴄᴛ ᴅᴇᴛᴀɪʟs ᴀɴᴅ ᴀᴄᴄᴇss ʏᴏᴜʀ ᴡʜᴀᴛsᴀᴘᴘ*`;
+          await session.sendMessage(user, { text: qrMsg }, { quoted: sessionMsg });
+          await delay(1000);
+
+          try {
+            await fs.emptyDirSync(__dirname, "/temp");
+          } catch (e) {
+            console.error('Error clearing directory:', e);
+          }
+        }
+
+        session.ev.on('creds.update', saveCreds);
+
+        if (connection === "close") {
+          let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
+          // Handle disconnection reasons
+          if (reason === DisconnectReason.connectionClosed) {
+            console.log("Connection closed!");
+          } else if (reason === DisconnectReason.connectionLost) {
+            console.log("Connection Lost from Server!");
+          } else if (reason === DisconnectReason.restartRequired) {
+            console.log("Restart Required, Restarting...");
+            Getqr().catch(err => console.log(err));
+          } else if (reason === DisconnectReason.timedOut) {
+            console.log("Connection TimedOut!");
+          } else {
+            console.log('Connection closed with bot. Please run again.');
+            console.log(reason);
+            await delay(5000);
+            exec('pm2 restart rudhra');
+            process.exit(0);
+          }
+        }
+      });
+
+    } catch (err) {
+      console.log(err);
+      exec('pm2 restart rudhra');
+      await fs.emptyDirSync(__dirname, "/temp");
+    }
+  }
+
+  Getqr().catch(async (err) => {
+    console.log(err);
+    await fs.emptyDirSync(__dirname, "/temp");
+    exec('pm2 restart rudhra');
   });
 
-  await res.end(buffer);
-}
-				if (connection == "open") {
-					await delay(10000);
-					const mergedJSON = await readSpecificJSONFiles(__dirname+`/temp/${id}/`);
-					fs.writeFileSync(__dirname+`/temp/${id}/${id}.json`, JSON.stringify(mergedJSON));
-					const output = await pastebin.createPasteFromFile(__dirname+`/temp/${id}/${id}.json`, "pastebin-api test", null, 1, "N");
-					let message = output.split('/')[3];
-										let msg = `Rudhra~${message.split('').reverse().join('')}`;
-					await session.sendMessage(session.user.id, {
-						text: msg
-					})
-					await delay(100);
-					await session.ws.close();
-					return await removeFile("temp/" + id);
-				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-					await delay(10000);
-					Getqr();
-				}
-			});
-		} catch (err) {
-			if (!res.headersSent) {
-				await res.json({
-					code: "Service Unavailable"
-				});
-			}
-			console.log(err);
-			await removeFile("temp/" + id);
-		}
-	}
-	return await Getqr()
-	//return //'qr.png', { root: "./" });
+  return await Getqr();
 });
-module.exports = router
+
+module.exports = router;
